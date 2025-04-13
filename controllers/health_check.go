9e773d391a8dfc80fb577157fa85dbe98fdb34f6
emmadal <emmanueldalougou@gmail.com>
2025-04-13 18:38:02 +0000

    fix: fix health check endpoint

--- controllers/health_check.go
+++ controllers/health_check.go
@@ -1,17 +1,19 @@
 package controllers
 
 import (
+	"net/http"
+	"time"
+
 	"github.com/gin-gonic/gin"
 )
 
 // HealthCheck handler to check health of the server
 func HealthCheck(c *gin.Context) {
-	// startTime := time.Now()
-	panic("panic ")
-	// c.SecureJSON(http.StatusOK, map[string]any{
-	// 	"status":    "ok",
-	// 	"message":   "Server is healthy",
-	// 	"timestamp": time.Now().Format(time.RFC3339),
-	// 	"uptime":    time.Since(startTime).String(),
-	// })
+	startTime := time.Now()
+	c.SecureJSON(http.StatusOK, map[string]any{
+		"status":    "ok",
+		"message":   "Server is healthy",
+		"timestamp": time.Now().Format(time.RFC3339),
+		"uptime":    time.Since(startTime).String(),
+	})
 }
