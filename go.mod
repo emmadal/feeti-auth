d31e5d64edac5e8f3ad202ab0f26df5cbb24eced
emmadal <emmanueldalougou@gmail.com>
2025-04-19 19:40:31 +0000

    chore: update dependencies and refactor endpoints

--- go.mod
+++ go.mod
@@ -1,35 +1,29 @@
 module github.com/emmadal/feeti-backend-user
 
-go 1.24.0
+go 1.24.2
 
 require (
-	github.com/emmadal/feeti-module v0.0.0-20250311191753-b6eee6c1650d
+	github.com/emmadal/feeti-module v0.0.0-20250415171343-af12d3ddc8e9
 	github.com/gin-contrib/cors v1.7.5
 	github.com/gin-contrib/gzip v1.2.3
 	github.com/gin-contrib/timeout v1.0.2
 	github.com/gin-gonic/gin v1.10.0
 	github.com/google/uuid v1.6.0
+	github.com/jackc/pgx/v5 v5.7.4
+	github.com/jeffotoni/quick v0.0.0-20250416215058-bd0ff9b52fe3
 	github.com/joho/godotenv v1.5.1
-	github.com/newrelic/go-agent/v3 v3.38.0
-	github.com/newrelic/go-agent/v3/integrations/nrgin v1.3.3
-	github.com/sirupsen/logrus v1.9.3
 	github.com/stretchr/testify v1.10.0
 	github.com/twilio/twilio-go v1.25.1
 	golang.org/x/crypto v0.37.0
 	golang.org/x/sync v0.13.0
-	gorm.io/driver/postgres v1.5.11
-	gorm.io/gorm v1.25.12
 )
 
 require (
-	github.com/bradfitz/gomemcache v0.0.0-20250403215159-8d39553ac7cf // indirect
 	github.com/bytedance/sonic v1.13.2 // indirect
 	github.com/bytedance/sonic/loader v0.2.4 // indirect
-	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/cloudwego/base64x v0.1.5 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
-	github.com/gabriel-vasile/mimetype v1.4.8 // indirect
+	github.com/gabriel-vasile/mimetype v1.4.9 // indirect
 	github.com/gin-contrib/sse v1.1.0 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
@@ -38,12 +32,10 @@ require (
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/golang-jwt/jwt/v5 v5.2.2 // indirect
 	github.com/golang/mock v1.6.0 // indirect
+	github.com/google/go-cmp v0.6.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
-	github.com/jackc/pgx/v5 v5.7.4 // indirect
 	github.com/jackc/puddle/v2 v2.2.2 // indirect
-	github.com/jinzhu/inflection v1.0.0 // indirect
-	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.10 // indirect
 	github.com/kr/pretty v0.3.1 // indirect
@@ -54,15 +46,12 @@ require (
 	github.com/pelletier/go-toml/v2 v2.2.4 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/redis/go-redis/v9 v9.7.3 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.12 // indirect
 	golang.org/x/arch v0.16.0 // indirect
 	golang.org/x/net v0.39.0 // indirect
 	golang.org/x/sys v0.32.0 // indirect
 	golang.org/x/text v0.24.0 // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20250409194420-de1ac958c67a // indirect
-	google.golang.org/grpc v1.71.1 // indirect
 	google.golang.org/protobuf v1.36.6 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -2,28 +2,24 @@ package main
 
 import (
 	"fmt"
+	"github.com/emmadal/feeti-backend-user/middleware"
 	"log"
 	"net/http"
 	"os"
 	"time"
 
 	"github.com/emmadal/feeti-backend-user/controllers"
-	"github.com/emmadal/feeti-backend-user/middleware"
 	"github.com/emmadal/feeti-backend-user/models"
-	"github.com/emmadal/feeti-module/cache"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-contrib/gzip"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
-	nrgin "github.com/newrelic/go-agent/v3/integrations/nrgin"
-	"github.com/newrelic/go-agent/v3/newrelic"
-	"github.com/sirupsen/logrus"
 )
 
 func main() {
 	// Load environment variables
 	if err := godotenv.Load(); err != nil {
-		logrus.WithFields(logrus.Fields{"warning": err}).Error("No .env file found")
+		log.Fatalln("Error loading .env file")
 	}
 
 	mode := os.Getenv("GIN_MODE")
@@ -37,41 +33,27 @@ func main() {
 	server := gin.Default()
 
 	// middleware
-	server.Use(cors.New(cors.Config{
-		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},
-		AllowOrigins:     []string{fmt.Sprintf("http://%s", os.Getenv("HOST"))},
-		AllowFiles:       false,
-		AllowWildcard:    false,
-		AllowCredentials: true,
-	}))
+	server.Use(
+		cors.New(
+			cors.Config{
+				AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},
+				AllowOrigins:     []string{"*"},
+				AllowFiles:       false,
+				AllowWildcard:    false,
+				AllowCredentials: true,
+			},
+		),
+	)
 	server.Use(middleware.Helmet())
 	server.Use(gzip.Gzip(gzip.BestCompression))
 	server.Use(middleware.Timeout(5 * time.Second))
 	server.Use(middleware.Recover())
-
-	// Initialize New Relic
-	app, err := newrelic.NewApplication(
-		newrelic.ConfigEnabled(true),
-		newrelic.ConfigAppName("backend-user"),
-		newrelic.ConfigLicense(os.Getenv("NEW_RELIC_LICENSE_KEY")),
-		newrelic.ConfigCodeLevelMetricsEnabled(true),
-		newrelic.ConfigAppLogForwardingEnabled(true),
-	)
-	if nil != err {
-		logrus.WithFields(logrus.Fields{"warning": err}).Warning("No newrelic instance found")
-	}
-	server.Use(nrgin.Middleware(app))
 
 	// Database connection
 	models.DBConnect()
 
 	// Set api version group
 	v1 := server.Group("/v1/api")
-
-	// Redis connection
-	if err := cache.InitRedis(); err != nil {
-		logrus.WithFields(logrus.Fields{"warning": err}).Warning("No redis instance found")
-	}
 
 	// initialize server
 	s := &http.Server{
@@ -88,15 +70,14 @@ func main() {
 	v1.POST("/new-otp", controllers.NewOTP)
 	v1.POST("/check-otp", controllers.CheckOTP)
 	v1.POST("/login", controllers.Login)
-	v1.POST("/reset-pin", controllers.ResetPin)
 	v1.PUT("/update-pin", controllers.UpdatePin)
-	v1.POST("/user", controllers.GetUser)
-	v1.GET("/health", controllers.HealthCheck)
-	v1.PUT("/update-profile", controllers.UpdateProfile)
 	v1.DELETE("/remove-account", controllers.RemoveAccount)
 	v1.DELETE("/signout", controllers.SignOut)
 
 	// start server
-	fmt.Fprintf(os.Stdout, "Server is running on port %s", os.Getenv("PORT"))
+	_, err := fmt.Fprintf(os.Stdout, "Server started on port %s\n", os.Getenv("PORT"))
+	if err != nil {
+		log.Fatalln("Error writing to stdout")
+	}
 	log.Fatalln(s.ListenAndServe())
 }
