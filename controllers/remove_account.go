ce20014b746c648bcfbc1ae7118b6997a4e01a94
emmadal <emmanueldalougou@gmail.com>
2025-05-14 14:42:06 +0000

    refactor: refactor remove account endpoint

--- controllers/remove_account.go
+++ controllers/remove_account.go
@@ -1,6 +1,8 @@
 package controllers
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
 	"os"
 
@@ -12,6 +14,7 @@ import (
 // RemoveAccount remove user account
 func RemoveAccount(c *gin.Context) {
 	body := models.RemoveUserAccount{}
+	var response helpers.RequestResponse
 
 	// Validate request body
 	if err := c.ShouldBindJSON(&body); err != nil {
@@ -32,13 +35,33 @@ func RemoveAccount(c *gin.Context) {
 		return
 	}
 
+	// publish a request to get wallet data
+	pMessage := helpers.ProducerMessage{
+		Subject: "wallet.lock",
+		Data:    fmt.Sprintf("%d", user.ID),
+	}
+	resp, err := pMessage.WalletEvent()
+	if err != nil {
+		helpers.HandleError(c, http.StatusInternalServerError, "Unable to process wallet", err)
+		return
+	}
+
+	// Unmarshal the wallet data
+	_ = json.Unmarshal(resp.Data, &response)
+	if !response.Success {
+		helpers.HandleError(c, http.StatusUnprocessableEntity, response.Error, nil)
+		return
+	}
+
 	// remove a user account
-	if err := user.RemoveUserAndWallet(); err != nil {
+	if err := user.DeactivateUserAccount(); err != nil {
 		helpers.HandleError(c, http.StatusInternalServerError, "Failed to remove account", err)
 		return
 	}
 
 	// Send success response and delete cookie
 	c.SetCookie("ftk", "", -1, "/", os.Getenv("HOST"), false, true)
+
+	// Send success response
 	helpers.HandleSuccess(c, "Account removed successfully")
 }
